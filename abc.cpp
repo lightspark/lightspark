f8d878f01834cd380dea3a41455b8d5c9ac65da0
alex <alex@beyond2.(none)>
2009-05-10 19:30:08 +0200

    First parsing of ABC block

--- abc.cpp
+++ abc.cpp
@@ -29,12 +29,239 @@ void ignore(istream& i, int count);
 
 DoABCTag::DoABCTag(RECORDHEADER h, std::istream& in):Tag(h,in)
 {
-	LOG(NOT_IMPLEMENTED,"DoABCTag");
-	skip(in);
+	int dest=in.tellg();
+	dest+=getSize();
+	in >> Flags >> Name;
+	LOG(CALLS,"DoABCTag Name: " << Name);
+
+	vm=new ABCVm(in);
+
+	if(dest!=in.tellg())
+		LOG(ERROR,"Corrupted ABC data: missing " << dest-in.tellg());
 }
 
 SymbolClassTag::SymbolClassTag(RECORDHEADER h, std::istream& in):Tag(h,in)
 {
 	LOG(NOT_IMPLEMENTED,"SymbolClassTag");
 	skip(in);
+}
+
+ABCVm::ABCVm(istream& in)
+{
+	in >> minor >> major;
+	LOG(CALLS,"ABCVm version " << major << '.' << minor);
+	in >> constant_pool;
+
+	char dump[4];
+	in.read(dump,4);
+	printf("ABC dump %x %x %x %x\n",dump[0],dump[1],dump[2],dump[3]);
+	in.read(dump,4);
+	printf("ABC dump %x %x %x %x\n",dump[0],dump[1],dump[2],dump[3]);
+}
+
+istream& operator>>(istream& in, u32& v)
+{
+	int i=0;
+	uint8_t t,t2;
+	v.val=0;
+	do
+	{
+		in.read((char*)&t,1);
+		t2=t;
+		t&=0x7f;
+
+		v.val|=(t<<i);
+		i+=7;
+		if(i==35)
+		{
+			if(t>15)
+				LOG(ERROR,"parsing u32");
+			break;
+		}
+	}
+	while(t2&0x80);
+	return in;
+}
+
+istream& operator>>(istream& in, s32& v)
+{
+	int i=0;
+	uint8_t t,t2;
+	v.val=0;
+	do
+	{
+		in.read((char*)&t,1);
+		t2=t;
+		t&=0x7f;
+
+		v.val|=(t<<i);
+		i+=7;
+		if(i==35)
+		{
+			if(t>15)
+				LOG(ERROR,"parsing s32");
+			break;
+		}
+	}
+	while(t2&0x80);
+	if(t2&0x40)
+	{
+		//Sign extend
+		for(i;i<32;i++)
+			v.val|=(1<<i);
+	}
+	return in;
+}
+
+istream& operator>>(istream& in, u30& v)
+{
+	int i=0;
+	uint8_t t,t2;
+	v.val=0;
+	do
+	{
+		in.read((char*)&t,1);
+		t2=t;
+		t&=0x7f;
+
+		v.val|=(t<<i);
+		i+=7;
+		if(i>29)
+			LOG(ERROR,"parsing u30");
+	}
+	while(t2&0x80);
+	return in;
+}
+
+istream& operator>>(istream& in, u8& v)
+{
+	uint8_t t;
+	in.read((char*)&t,1);
+	v.val=t;
+	return in;
+}
+
+istream& operator>>(istream& in, u16& v)
+{
+	uint16_t t;
+	in.read((char*)&t,2);
+	v.val=t;
+	return in;
+}
+
+istream& operator>>(istream& in, d64& v)
+{
+	//Should check if this is right
+	in.read((char*)&v.val,8);
+	return in;
+}
+
+istream& operator>>(istream& in, string_info& v)
+{
+	in >> v.size;
+	//TODO: String are expected to be UTF-8 encoded.
+	//This temporary implementation assume ASCII, so fail if high bit is set
+	uint8_t t;
+	v.val.reserve(v.size);
+	for(int i=0;i<v.size;i++)
+	{
+		in.read((char*)&t,1);
+		v.val.push_back(t);
+		if(t&0x80)
+			LOG(NOT_IMPLEMENTED,"Multibyte not handled");
+	}
+	return in;
+}
+
+istream& operator>>(istream& in, namespace_info& v)
+{
+	in >> v.kind >> v.name;
+	if(v.kind!=0x05 && v.kind!=0x08 && v.kind!=0x16 && v.kind!=0x17 && v.kind!=0x18 && v.kind!=0x19 && v.kind!=0x1a)
+		LOG(ERROR,"Unexpected namespace kind");
+	return in;
+}
+
+istream& operator>>(istream& in, ns_set_info& v)
+{
+	in >> v.count;
+
+	v.ns.resize(v.count);
+	for(int i=0;i<v.count;i++)
+	{
+		in >> v.ns[i];
+		if(v.ns[i]==0)
+			LOG(ERROR,"0 not allowed");
+	}
+	return in;
+}
+
+istream& operator>>(istream& in, multiname_info& v)
+{
+	in >> v.kind;
+
+	switch(v.kind)
+	{
+		case 0x07:
+		case 0x0d:
+			in >> v.ns >> v.name;
+			break;
+		case 0x0f:
+		case 0x10:
+			in >> v.name;
+			break;
+		case 0x11:
+		case 0x12:
+			break;
+		case 0x09:
+		case 0x0e:
+			in >> v.name >> v.ns_set;
+			break;
+		case 0x1b:
+		case 0x1c:
+			in >> v.ns_set;
+			break;
+		default:
+			LOG(ERROR,"Unexpected multiname kind");
+			break;
+	}
+}
+
+istream& operator>>(istream& in, cpool_info& v)
+{
+	in >> v.int_count;
+	v.integer.resize(v.int_count);
+	for(int i=1;i<v.int_count;i++)
+		in >> v.integer[i];
+
+	in >> v.uint_count;
+	v.uinteger.resize(v.uint_count);
+	for(int i=1;i<v.uint_count;i++)
+		in >> v.uinteger[i];
+
+	in >> v.double_count;
+	v.doubles.resize(v.double_count);
+	for(int i=1;i<v.double_count;i++)
+		in >> v.doubles[i];
+
+	in >> v.string_count;
+	v.strings.resize(v.string_count);
+	for(int i=1;i<v.string_count;i++)
+		in >> v.strings[i];
+
+	in >> v.namespace_count;
+	v.namespaces.resize(v.namespace_count);
+	for(int i=1;i<v.namespace_count;i++)
+		in >> v.namespaces[i];
+
+	in >> v.ns_set_count;
+	v.ns_sets.resize(v.ns_set_count);
+	for(int i=1;i<v.ns_set_count;i++)
+		in >> v.ns_sets[i];
+
+	in >> v.multiname_count;
+	v.multinames.resize(v.multiname_count);
+	for(int i=1;i<v.multiname_count;i++)
+		in >> v.multinames[i];
+
+	return in;
 }
--- abc.h
+++ abc.h
@@ -22,10 +22,132 @@
 #include "logger.h"
 #include <vector>
 
+class s32
+{
+friend std::istream& operator>>(std::istream& in, s32& v);
+private:
+	int32_t val;
+public:
+	operator int32_t(){return val;}
+};
+
+class u32
+{
+friend std::istream& operator>>(std::istream& in, u32& v);
+private:
+	uint32_t val;
+public:
+	operator uint32_t(){return val;}
+};
+
+class u30
+{
+friend std::istream& operator>>(std::istream& in, u30& v);
+private:
+	uint32_t val;
+public:
+	operator uint32_t(){return val;}
+};
+
+class u16
+{
+friend std::istream& operator>>(std::istream& in, u16& v);
+private:
+	uint32_t val;
+public:
+	operator uint32_t(){return val;}
+};
+
+class u8
+{
+friend std::istream& operator>>(std::istream& in, u8& v);
+private:
+	uint32_t val;
+public:
+	operator uint32_t(){return val;}
+};
+
+class d64
+{
+friend std::istream& operator>>(std::istream& in, d64& v);
+private:
+	double val;
+public:
+	operator double(){return val;}
+};
+
+class string_info
+{
+friend std::istream& operator>>(std::istream& in, string_info& v);
+private:
+	u30 size;
+	std::string val;
+public:
+	operator std::string(){return val;}
+};
+
+class namespace_info
+{
+friend std::istream& operator>>(std::istream& in, namespace_info& v);
+private:
+	u8 kind;
+	u30 name;
+};
+
+class ns_set_info
+{
+friend std::istream& operator>>(std::istream& in, ns_set_info& v);
+private:
+	u30 count;
+	std::vector<u30> ns;
+};
+
+class multiname_info
+{
+friend std::istream& operator>>(std::istream& in, multiname_info& v);
+private:
+	u8 kind;
+	u30 name;
+	u30 ns;
+	u30 ns_set;
+};
+
+class cpool_info
+{
+friend std::istream& operator>>(std::istream& in, cpool_info& v);
+private:
+	u30 int_count;
+	std::vector<s32> integer;
+	u30 uint_count;
+	std::vector<u32> uinteger;
+	u30 double_count;
+	std::vector<d64> doubles;
+	u30 string_count;
+	std::vector<string_info> strings;
+	u30 namespace_count;
+	std::vector<namespace_info> namespaces;
+	u30 ns_set_count;
+	std::vector<ns_set_info> ns_sets;
+	u30 multiname_count;
+	std::vector<multiname_info> multinames;
+};
+
+class ABCVm
+{
+private:
+	u16 minor;
+	u16 major;
+	cpool_info constant_pool;
+public:
+	ABCVm(std::istream& in);
+};
+
 class DoABCTag: public Tag
 {
 private:
-
+	UI32 Flags;
+	STRING Name;
+	ABCVm* vm;
 public:
 	DoABCTag(RECORDHEADER h, std::istream& in);
 };
@@ -35,3 +157,16 @@ class SymbolClassTag: public Tag
 public:
 	SymbolClassTag(RECORDHEADER h, std::istream& in);
 };
+
+std::istream& operator>>(std::istream& in, u8& v);
+std::istream& operator>>(std::istream& in, u16& v);
+std::istream& operator>>(std::istream& in, u30& v);
+std::istream& operator>>(std::istream& in, u32& v);
+std::istream& operator>>(std::istream& in, s32& v);
+std::istream& operator>>(std::istream& in, d64& v);
+std::istream& operator>>(std::istream& in, string_info& v);
+std::istream& operator>>(std::istream& in, namespace_info& v);
+std::istream& operator>>(std::istream& in, ns_set_info& v);
+std::istream& operator>>(std::istream& in, multiname_info& v);
+std::istream& operator>>(std::istream& in, cpool_info& v);
+

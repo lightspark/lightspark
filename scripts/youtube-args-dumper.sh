2092fd6658b54d636bb21d4db0874668529680a7
Alessandro <a.pignotti@sssup.it>
2010-04-01 17:28:00 +0200

    Updated dumper for youtube args

--- scripts/youtube-args-dumper.sh
+++ scripts/youtube-args-dumper.sh
@@ -4,4 +4,6 @@ echo "Dumping from " $1 >&2
 
 wget $1 -q -O tmp
 
-cat tmp | grep SWF_ARGS | sed "s/.*{\(.*\)}.*/\1/g" | sed "s/\", /\"\n/g" | grep -v rv.[0-9] | sed "s/\"\(.*\)\": \"\(.*\)\"/\1\n\2/g" | ./urldecode.sh
+#cat tmp | grep SWF_ARGS | sed "s/.*{\(.*\)}.*/\1/g" | sed "s/\", /\"\n/g" | grep -v rv.[0-9] | sed "s/\"\(.*\)\": \"\(.*\)\"/\1\n\2/g" | ./urldecode.sh
+
+cat tmp | egrep -o "<embed(.*?)/>" | sed "s/ /\n/g" | grep flashvars | grep -o '\\\".*\\\"' | sed 's@\\\"\(.*\)\\\"@\1@g' | sed "s/&/\n/g" | sed "s/=/\n/g" | ./urldecode.sh

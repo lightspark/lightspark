a4645fe53d6aa0812531a5650446f4aad89f0f1f
Alessandro <a.pignotti@sssup.it>
2010-06-19 22:45:41 +0200

    Revert "Removed deprecated functions (from avcodec.h) and replaced by the new ones."
    This reverts commit 7bb38b5f716648cdb883aa6e340b86d2a61ae1bb.
    Ubuntu lucid ships with an older ffmpeg

--- decoder.cpp
+++ decoder.cpp
@@ -107,11 +107,7 @@ bool FFMpegVideoDecoder::discardFrame()
 bool FFMpegVideoDecoder::decodeData(uint8_t* data, uint32_t datalen)
 {
 	int frameOk=0;
-	AVPacket pkt;
-	av_init_packet(&pkt);
-	pkt.data= data;
-	pkt.size= datalen;
-	avcodec_decode_video2(codecContext, frameIn, &frameOk, &pkt);
+	avcodec_decode_video(codecContext, frameIn, &frameOk, data, datalen);
 	if(frameOk==0)
 		throw RunTimeException("Cannot decode frame");
 	assert(codecContext->pix_fmt==PIX_FMT_YUV420P);
@@ -249,11 +245,7 @@ bool FFMpegAudioDecoder::decodeData(uint8_t* data, uint32_t datalen)
 {
 	FrameSamples& curTail=samplesBuffer.acquireLast();
 	int maxLen=AVCODEC_MAX_AUDIO_FRAME_SIZE;
-	AVPacket pkt;
-	av_init_packet(&pkt);
-	pkt.data=data;
-	pkt.size=datalen;
-	uint32_t ret=avcodec_decode_audio3(codecContext, curTail.samples, &maxLen, &pkt);
+	uint32_t ret=avcodec_decode_audio2(codecContext, curTail.samples, &maxLen, data, datalen);
 	assert_and_throw(ret==datalen);
 	curTail.len=maxLen;
 	assert(maxLen%2==0);

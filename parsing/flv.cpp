5bf6d1b1fe0a03608d38d7237a5ad453e508286b
Thomas Dziedzic <gostrc@gmail.com>
2010-08-15 07:36:15 -0500

    Revert "fix warnings"
    This reverts commit d2bdc925f06d26dcd0290fde84dd378d67fa579b.

--- parsing/flv.cpp
+++ parsing/flv.cpp
@@ -203,10 +203,8 @@ VideoDataTag::VideoDataTag(istream& s):VideoTag(s),_isHeader(false),packetData(N
 		//Is everything raw?
 		//Compute lenght of raw data
 		packetLen=dataSize-1;
-#ifndef NDEBUG
 		int ret=aligned_malloc((void**)&packetData, 16, packetLen+16); //Ensure no overrun happens when doing aligned reads
 		assert(ret==0);
-#endif
 
 		s.read((char*)packetData,packetLen);
 		memset(packetData+packetLen,0,16);
@@ -235,10 +233,8 @@ VideoDataTag::VideoDataTag(istream& s):VideoTag(s),_isHeader(false),packetData(N
 
 		//Compute lenght of raw data
 		packetLen=dataSize-5;
-#ifndef NDEBUG
 		int ret=aligned_malloc((void**)&packetData, 16, packetLen+16); //Ensure no overrun happens when doing aligned reads
 		assert(ret==0);
-#endif
 
 		s.read((char*)packetData,packetLen);
 		memset(packetData+packetLen,0,16);
@@ -287,10 +283,8 @@ AudioDataTag::AudioDataTag(std::istream& s):VideoTag(s),_isHeader(false)
 		headerConsumed++;
 	}
 	packetLen=dataSize-headerConsumed;
-#ifndef NDEBUG
 	int ret=aligned_malloc((void**)&packetData, 16, packetLen+16); //Ensure no overrun happens when doing aligned reads
 	assert(ret==0);
-#endif
 	s.read((char*)packetData,packetLen);
 	memset(packetData+packetLen,0,16);
 

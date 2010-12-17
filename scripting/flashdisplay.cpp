e8a50b7cacbe0518c32282d8c2e12fbf804945a5
Alessandro Pignotti <a.pignotti@sssup.it>
2010-12-17 05:52:39 +0100

    Only support GET method in Loader

--- scripting/flashdisplay.cpp
+++ scripting/flashdisplay.cpp
@@ -183,6 +183,7 @@ ASFUNCTIONBODY(Loader,load)
 	assert_and_throw(argslen > 0 && args[0] && argslen <= 2);
 	assert_and_throw(args[0]->getPrototype()->isSubClass(Class<URLRequest>::getClass()));
 	URLRequest* r=static_cast<URLRequest*>(args[0]);
+	assert_and_throw(r->method==URLRequest::GET);
 	th->url=r->url;
 	th->source=URL;
 	//To be decreffed in jobFence

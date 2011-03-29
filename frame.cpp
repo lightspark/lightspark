7d973e26ece8fb79a4db67d94f54a1a188e74f1f
Alessandro Pignotti <a.pignotti@sssup.it>
2011-03-29 03:34:23 +0200

    Remove useless syncronization

--- frame.cpp
+++ frame.cpp
@@ -105,21 +105,6 @@ void Frame::init(MovieClip* parent, const list <pair<PlaceInfo, DisplayObject*> 
 			for(unsigned int i=0;i<controls.size();i++)
 				controls[i]->execute(parent->getRoot());
 			controls.clear();
-
-			if(sys->currentVm)
-			{
-				//We stop execution until execution engine catches up
-				SynchronizationEvent* se=new SynchronizationEvent;
-				bool added=sys->currentVm->addEvent(NULL, se);
-				if(!added)
-				{
-					se->decRef();
-					throw RunTimeException("Could not add event");
-				}
-				se->wait();
-				se->decRef();
-				//Now the bindings are effective for all tags
-			}
 		}
 
 		displayList=d;

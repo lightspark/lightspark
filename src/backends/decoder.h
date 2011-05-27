7331186cddc373de8c5a138430f04752193c67a5
Jani Monoses <jani@ubuntu.com>
2011-05-27 13:10:37 +0300

    Fix warning with newer libav versions

--- src/backends/decoder.h
+++ src/backends/decoder.h
@@ -326,7 +326,11 @@ private:
 	uint8_t avioBuffer[4096];
 	static int avioReadPacket(void* t, uint8_t* buf, int buf_size);
 	//NOTE: this will become AVIOContext in FFMpeg 0.7
+#if LIBAVUTIL_VERSION_MAJOR < 51
 	ByteIOContext* avioContext;
+#else
+	AVIOContext* avioContext;
+#endif
 public:
 	FFMpegStreamDecoder(std::istream& s);
 	~FFMpegStreamDecoder();

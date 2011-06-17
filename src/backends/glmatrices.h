3a25ef22c74ba469c7034ecc48b11721589aa7e1
Jani Monoses <jani@ubuntu.com>
2011-06-17 13:53:54 +0300

    Add copyright header and multiple include protection

--- src/backends/glmatrices.h
+++ src/backends/glmatrices.h
@@ -1,3 +1,25 @@
+/**************************************************************************
+    Lightspark, a free flash player implementation
+
+    Copyright (C) 2009-2011  Alessandro Pignotti (a.pignotti@sssup.it)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#ifndef GLMATRICES_H
+#define GLMATRICES_H
+
 #define LSGL_MATRIX_SIZE (16*sizeof(GLfloat))
 
 extern GLfloat lsIdentityMatrix[16];
@@ -10,3 +32,5 @@ void lsglMultMatrixf(const GLfloat *m);
 void lsglScalef(GLfloat scaleX, GLfloat scaleY, GLfloat scaleZ);
 void lsglTranslatef(GLfloat translateX, GLfloat translateY, GLfloat translateZ);
 void lsglOrtho(GLfloat l, GLfloat r, GLfloat b, GLfloat t, GLfloat n, GLfloat f);
+
+#endif

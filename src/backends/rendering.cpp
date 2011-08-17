6c1bee7744e6634d0d9d27b76796c4a68f111ff3
Jani Monoses <jani@ubuntu.com>
2011-08-17 17:41:40 +0300

    Remove EGL setup debug message

--- src/backends/rendering.cpp
+++ src/backends/rendering.cpp
@@ -317,7 +317,6 @@ void* RenderThread::worker(RenderThread* th)
 	{
 		EGLint id;
 		eglGetConfigAttrib(ed, conf[i], EGL_NATIVE_VISUAL_ID, &id);
-		LOG(LOG_ERROR, id <<" -> "<<e->visual);
 		if(id==(int)e->visual)
 			break;
 	}

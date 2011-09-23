3cb6086ee562f1f56222d54e476c7cdeac6db7f5
Jani Monoses <jani@ubuntu.com>
2011-09-23 14:53:57 +0300

    Split out Date.cpp from toplevel.cpp

---
+++ src/scripting/toplevel/Date.cpp
@@ -0,0 +1,220 @@
+/**************************************************************************
+    Lightspark, a free flash player implementation
+
+    Copyright (C) 2009-2011  Alessandro Pignotti (a.pignotti@sssup.it)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#include "Date.h"
+#include "class.h"
+
+using namespace std;
+using namespace Glib;
+using namespace lightspark;
+
+SET_NAMESPACE("");
+REGISTER_CLASS_NAME(Date);
+
+Date::Date():year(-1),month(-1),date(-1),hour(-1),minute(-1),second(-1),millisecond(-1)
+{
+}
+
+void Date::sinit(Class_base* c)
+{
+	c->super=Class<ASObject>::getClass();
+	c->max_level=c->super->max_level+1;
+	c->setConstructor(Class<IFunction>::getFunction(_constructor));
+	c->setDeclaredMethodByQName("getTimezoneOffset",AS3,Class<IFunction>::getFunction(getTimezoneOffset),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("valueOf",AS3,Class<IFunction>::getFunction(valueOf),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getTime",AS3,Class<IFunction>::getFunction(getTime),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getFullYear",AS3,Class<IFunction>::getFunction(getFullYear),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getHours",AS3,Class<IFunction>::getFunction(getHours),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getMinutes",AS3,Class<IFunction>::getFunction(getMinutes),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getMilliseconds",AS3,Class<IFunction>::getFunction(getMilliseconds),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getSeconds",AS3,Class<IFunction>::getFunction(getMinutes),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getUTCFullYear",AS3,Class<IFunction>::getFunction(getUTCFullYear),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getUTCMonth",AS3,Class<IFunction>::getFunction(getUTCMonth),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getUTCDate",AS3,Class<IFunction>::getFunction(getUTCDate),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getUTCHours",AS3,Class<IFunction>::getFunction(getUTCHours),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("getUTCMinutes",AS3,Class<IFunction>::getFunction(getUTCMinutes),NORMAL_METHOD,true);
+	c->setDeclaredMethodByQName("fullYear","",Class<IFunction>::getFunction(getFullYear),GETTER_METHOD,true);
+	//o->setVariableByQName("toString",AS3,Class<IFunction>::getFunction(ASObject::_toString));
+}
+
+void Date::buildTraits(ASObject* o)
+{
+}
+
+ASFUNCTIONBODY(Date,_constructor)
+{
+	Date* th=static_cast<Date*>(obj);
+	th->year=1969;
+	th->month=1;
+	th->date=1;
+	th->hour=0;
+	th->minute=0;
+	th->second=0;
+	th->millisecond=0;
+	return NULL;
+}
+
+ASFUNCTIONBODY(Date,getTimezoneOffset)
+{
+	LOG(LOG_NOT_IMPLEMENTED,_("getTimezoneOffset"));
+	return abstract_d(120);
+}
+
+ASFUNCTIONBODY(Date,getUTCFullYear)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->year);
+}
+
+ASFUNCTIONBODY(Date,getUTCMonth)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->month);
+}
+
+ASFUNCTIONBODY(Date,getUTCDate)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->date);
+}
+
+ASFUNCTIONBODY(Date,getUTCHours)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->hour);
+}
+
+ASFUNCTIONBODY(Date,getUTCMinutes)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->minute);
+}
+
+ASFUNCTIONBODY(Date,getFullYear)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->year);
+}
+
+ASFUNCTIONBODY(Date,getHours)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->hour);
+}
+
+ASFUNCTIONBODY(Date,getMinutes)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->minute);
+}
+
+ASFUNCTIONBODY(Date,getMilliseconds)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->millisecond);
+}
+
+ASFUNCTIONBODY(Date,getTime)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->toInt());
+}
+
+ASFUNCTIONBODY(Date,valueOf)
+{
+	Date* th=static_cast<Date*>(obj);
+	return abstract_d(th->toInt());
+}
+
+bool Date::getIsLeapYear(int year)
+{
+	return ( ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) );
+}
+
+int Date::getDaysInMonth(int month, bool isLeapYear)
+{
+	enum { JANUARY = 1, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER };
+
+	int days;
+
+	switch(month)
+	{
+	case FEBRUARY:
+		days = isLeapYear ? 29 : 28;
+		break;
+	case JANUARY:
+	case MARCH:
+	case MAY:
+	case JULY:
+	case AUGUST:
+	case OCTOBER:
+	case DECEMBER:
+		days = 31;
+		break;
+	case APRIL:
+	case JUNE:
+	case SEPTEMBER:
+	case NOVEMBER:
+		days = 30;
+		break;
+	default:
+		days = -1;
+	}
+
+	return days;
+}
+
+int Date::toInt()
+{
+	int ret=0;
+
+	ret+=((year-1990)*365 + ((year-1989)/4 - (year-1901)/100) + (year-1601)/400)*24*3600*1000;
+
+	bool isLeapYear;
+	for(int j = 1; j < month; j++)
+	{
+		isLeapYear = getIsLeapYear(year);
+		ret+=getDaysInMonth(j, isLeapYear)*24*3600*1000;
+	}
+
+	ret+=(date-1)*24*3600*1000;
+	ret+=hour*3600*1000;
+	ret+=minute*60*1000;
+	ret+=second*1000;
+	ret+=millisecond;
+	return ret;
+}
+
+tiny_string Date::toString(bool debugMsg)
+{
+	assert_and_throw(implEnable);
+	return toString_priv();
+}
+
+tiny_string Date::toString_priv() const
+{
+	return "Wed Dec 31 16:00:00 GMT-0800 1969";
+}
+
+void Date::serialize(ByteArray* out, std::map<tiny_string, uint32_t>& stringMap,
+				std::map<const ASObject*, uint32_t>& objMap) const
+{
+	throw UnsupportedException("Date::serialize not implemented");
+}
+

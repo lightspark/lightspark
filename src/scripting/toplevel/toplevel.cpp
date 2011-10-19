a75bcca9578fdfe921ba4bac6e6bd1c3713ddbca
Jasper St. Pierre <jstpierre@mecheye.net>
2011-10-19 13:58:40 -0400

    toplevel: Use glib's ASCII string functions so we aren't dependent on locale

--- src/scripting/toplevel/toplevel.cpp
+++ src/scripting/toplevel/toplevel.cpp
@@ -33,6 +33,8 @@
 #include <ctype.h>
 #include <errno.h>
 
+#include <glib/glib.h>
+
 #include "abc.h"
 #include "toplevel.h"
 #include "flash/events/flashevents.h"
@@ -1530,13 +1532,15 @@ double ASString::toNumber() const
 
 	const char *s=data.c_str();
 	char *end=NULL;
-	double val=strtod(s, &end);
+	while(g_ascii_isspace(*s))
+		s++;
+	double val=g_ascii_strtod(s, &end);
 
 	// strtod converts case-insensitive "inf" and "infinity" to
 	// inf, flash only accepts case-sensitive "Infinity".
 	if(std::isinf(val)) {
 		const char *tmp=s;
-		while(isspace(*tmp))
+		while(g_ascii_isspace(*tmp))
 			tmp++;
 		if(*tmp=='+' || *tmp=='-')
 			tmp++;
@@ -1545,9 +1549,8 @@ double ASString::toNumber() const
 	}
 
 	// Fail if there is any rubbish after the converted number
-	while(*end)
-	{
-		if(!isspace(*end))
+	while(*end) {
+		if(!g_ascii_isspace(*end))
 			return numeric_limits<double>::quiet_NaN();
 		end++;
 	}
@@ -1558,7 +1561,7 @@ double ASString::toNumber() const
 int32_t ASString::toInt()
 {
 	assert_and_throw(implEnable);
-	return atol(data.c_str());
+	return atoi(data.c_str());
 }
 
 uint32_t ASString::toUInt()
@@ -3969,7 +3972,7 @@ ASFUNCTIONBODY(lightspark,parseInt)
 
 		errno=0;
 		char *end;
-		long val=strtol(cur, &end, radix);
+		int64_t val=g_ascii_strtoll(cur, &end, radix);
 
 		if(errno==ERANGE)
 		{

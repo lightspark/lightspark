e517e9be106f18ad11b085a705895e64c28c1062
Matthias Gehre <M.Gehre@gmx.de>
2011-10-21 22:46:17 +0200

    insertLegacyChildAt: fix last commit

--- src/scripting/flash/display/flashdisplay.cpp
+++ src/scripting/flash/display/flashdisplay.cpp
@@ -2017,7 +2017,7 @@ void DisplayObjectContainer::insertLegacyChildAt(uint32_t depth, DisplayObject* 
 		if(hasPropertyByMultiname(objName,true))
 			setVariableByMultiname(objName,obj);
 		else
-			obj->setVariableByQName(objName.name_s,objName.ns[0],obj,DECLARED_TRAIT);
+			setVariableByQName(objName.name_s,objName.ns[0],obj,DECLARED_TRAIT);
 	}
 
 	depthToLegacyChild.insert(boost::bimap<uint32_t,DisplayObject*>::value_type(depth,obj));

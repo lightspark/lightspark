3fe65d99064ef58d3c99d07f0fb62652da29fd26
Matthias Gehre <M.Gehre@gmx.de>
2011-11-15 21:45:58 +0100

    plugin: initialize gtk
    This is needed on win32, because there firefox doesn't use gtk itself

--- src/plugin/plugin.cpp
+++ src/plugin/plugin.cpp
@@ -201,7 +201,13 @@ char* NPP_GetMIMEDescription(void)
 NPError NS_PluginInitialize()
 {
 	LOG_LEVEL log_level = LOG_INFO;
+
+	/* setup glib/gtk, this is already done on firefox/linux, but needs to be done
+	 * on firefox/windows */
 	g_thread_init(NULL);
+	gdk_threads_init();
+	gtk_init(NULL, NULL);
+
 	char *envvar = getenv("LIGHTSPARK_PLUGIN_LOGLEVEL");
 	if (envvar)
 		log_level=(LOG_LEVEL) min(4, max(0, atoi(envvar)));

lightspark/lightspark

commit bf7c215b9b9c84de4904f1589c9a02b7b6eadf31

Delay control tag execution to until the whole SWF file has been parsed.

src/swf.cpp
@@ -1232,8 +1232,10 @@ void ParseThread::parseSWF(UI8 ver)
 					//fall through
 				case ABC_TAG:
 				{
+					// Add control tags to the queue, to be executed when the rest of the 
+					// SWF has been parsed.
 					_R<ControlTag> ctag = tag.cast<ControlTag>();
-					ctag->execute(root);
+					controlTags.push(ctag);
 					break;
 				}
 				case FRAMELABEL_TAG:
@@ -1257,6 +1259,15 @@ void ParseThread::parseSWF(UI8 ver)
 		throw;
 	}
 	LOG(LOG_INFO,_("End of parsing"));
+
+	// The whole SWF has been parsed, now execute all control tags, in the order
+	// in which they appeared in the file.
+	while(!controlTags.empty())
+	{
+		_R<ControlTag> ctag = controlTags.front();
+		controlTags.pop();
+		ctag->execute(root);
+	}
 }
 
 void ParseThread::parseBitmap()

src/swf.h
@@ -23,6 +23,7 @@
 #include "compat.h"
 #include <fstream>
 #include <list>
+#include <queue>
 #include <map>
 #include <string>
 #include "swftypes.h"
@@ -39,6 +40,7 @@ namespace lightspark
 class ABCVm;
 class AudioManager;
 class Config;
+class ControlTag;
 class DownloadManager;
 class DisplayListTag;
 class DictionaryTag;
@@ -370,6 +372,7 @@ private:
 	Spinlock objectSpinlock;
 	tiny_string url;
 	FILE_TYPE fileType;
+	std::queue<_R<ControlTag>> controlTags;
 	void threadAbort();
 	void jobFence() {};
 	void parseSWFHeader(RootMovieClip *root, UI8 ver);

commit 7447059b14a2e3a14db6d30319c4ce03105ef584

Try to fix problems introduced by commit bf7c215b9b9 by only delaying SymbolClassTags and not all ControlTags in general.

src/parsing/tags.cpp
@@ -344,6 +344,7 @@ DefineSpriteTag::DefineSpriteTag(RECORDHEADER h, std::istream& in):DictionaryTag
 				empty=true;
 				break;
 			}
+			case SYMBOL_CLASS_TAG:
 			case ABC_TAG:
 			case CONTROL_TAG:
 				throw ParseException("Control tag inside a sprite. Should not happen.");

src/parsing/tags.h
@@ -34,7 +34,7 @@
 namespace lightspark
 {
 
-enum TAGTYPE {TAG=0,DISPLAY_LIST_TAG,SHOW_TAG,CONTROL_TAG,DICT_TAG,FRAMELABEL_TAG,ABC_TAG,END_TAG};
+enum TAGTYPE {TAG=0,DISPLAY_LIST_TAG,SHOW_TAG,CONTROL_TAG,DICT_TAG,FRAMELABEL_TAG,SYMBOL_CLASS_TAG,ABC_TAG,END_TAG};
 
 void ignore(std::istream& i, int count);
 

src/scripting/abc.h
@@ -513,7 +513,7 @@ private:
 	std::vector<STRING> Names;
 public:
 	SymbolClassTag(RECORDHEADER h, std::istream& in);
-	TAGTYPE getType() const{ return ABC_TAG; }
+	TAGTYPE getType() const{ return SYMBOL_CLASS_TAG; }
 	void execute(RootMovieClip* root);
 };
 

src/swf.cpp
@@ -1195,6 +1195,14 @@ void ParseThread::parseSWF(UI8 ver)
 			{
 				case END_TAG:
 				{
+					// The whole frame has been parsed, now execute all queued SymbolClass tags,
+					// in the order in which they appeared in the file.
+					while(!symbolClassTags.empty())
+					{
+						symbolClassTags.front()->execute(root);
+						symbolClassTags.pop();
+					}
+
 					if(!empty)
 						root->commitFrame(false);
 					else
@@ -1216,9 +1224,27 @@ void ParseThread::parseSWF(UI8 ver)
 					empty=false;
 					break;
 				case SHOW_TAG:
+					// The whole frame has been parsed, now execute all queued SymbolClass tags,
+					// in the order in which they appeared in the file.
+					while(!symbolClassTags.empty())
+					{
+						symbolClassTags.front()->execute(root);
+						symbolClassTags.pop();
+					}
+
 					root->commitFrame(true);
 					empty=true;
 					break;
+				case SYMBOL_CLASS_TAG:
+				{
+					// Add symbol class tags to the queue, to be executed when the rest of the 
+					// frame has been parsed. This is to handle invalid SWF files that define ID's
+					// used in the SymbolClass tag only after the tag, which would otherwise result
+					// in "undefined dictionary ID" errors.
+					_R<ControlTag> stag = tag.cast<ControlTag>();
+					symbolClassTags.push(stag);
+					break;
+				}
 				case CONTROL_TAG:
 					/* The spec is not clear about that,
 					 * but it seems that all the CONTROL_TAGs
@@ -1232,10 +1258,8 @@ void ParseThread::parseSWF(UI8 ver)
 					//fall through
 				case ABC_TAG:
 				{
-					// Add control tags to the queue, to be executed when the rest of the 
-					// SWF has been parsed.
 					_R<ControlTag> ctag = tag.cast<ControlTag>();
-					controlTags.push(ctag);
+					ctag->execute(root);
 					break;
 				}
 				case FRAMELABEL_TAG:
@@ -1259,15 +1283,6 @@ void ParseThread::parseSWF(UI8 ver)
 		throw;
 	}
 	LOG(LOG_INFO,_("End of parsing"));
-
-	// The whole SWF has been parsed, now execute all control tags, in the order
-	// in which they appeared in the file.
-	while(!controlTags.empty())
-	{
-		_R<ControlTag> ctag = controlTags.front();
-		controlTags.pop();
-		ctag->execute(root);
-	}
 }
 
 void ParseThread::parseBitmap()

src/swf.h
@@ -372,7 +372,7 @@ private:
 	Spinlock objectSpinlock;
 	tiny_string url;
 	FILE_TYPE fileType;
-	std::queue<_R<ControlTag>> controlTags;
+	std::queue<_R<ControlTag>> symbolClassTags;
 	void threadAbort();
 	void jobFence() {};
 	void parseSWFHeader(RootMovieClip *root, UI8 ver);
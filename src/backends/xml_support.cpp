96aaa4eb28e51b857b188ecae6cd0ef32dc9ceda
Alessandro Pignotti <a.pignotti@sssup.it>
2012-04-20 18:17:07 +0200

    Fix the build for slightly older libxml++ releases

--- src/backends/xml_support.cpp
+++ src/backends/xml_support.cpp
@@ -17,6 +17,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 
+#include <libxml++/nodes/textnode.h>
 #include "backends/xml_support.h"
 #include "logger.h"
 

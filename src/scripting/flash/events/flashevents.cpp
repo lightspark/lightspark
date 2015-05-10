5021005a44f3595fdc459858e07c1a1bd5d260c9
Ludger Krämer <dbluelle@onlinehome.de>
2015-05-10 10:11:48 +0200

    [EventDispatcher] allow empty event type

--- src/scripting/flash/events/flashevents.cpp
+++ src/scripting/flash/events/flashevents.cpp
@@ -622,7 +622,6 @@ ASFUNCTIONBODY(EventDispatcher,dispatchEvent)
 
 	args[0]->incRef();
 	_R<Event> e=_MR(Class<Event>::cast(args[0]));
-	assert_and_throw(e->type!="");
 
 	// Must call the AS getter, because the getter may have been
 	// overridden

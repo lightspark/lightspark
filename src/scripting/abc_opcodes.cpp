1fca138e481c7e515b48a168c536ff5a172e7646
Ludger Krämer <dbluelle@onlinehome.de>
2017-03-26 17:53:32 +0200

    [ABCVm] fix possible crash in coerce

--- src/scripting/abc_opcodes.cpp
+++ src/scripting/abc_opcodes.cpp
@@ -187,6 +187,11 @@ void ABCVm::coerce(call_context* th, int n)
 	ASObject** o=th->runtime_stack_pointer();
 	
 	const Type* type = Type::getTypeFromMultiname(mn, th->context);
+	if (type == NULL)
+	{
+		LOG(LOG_ERROR,"coerce: type not found:"<< *mn);
+		throwError<TypeError>(kClassNotFoundError,mn->qualifiedString(getSys()));
+	}
 	*o=type->coerce(*o);
 }
 

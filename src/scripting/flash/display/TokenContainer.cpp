lightspark/lightspark

commit f7964bf80eceee3edef2612df00e988f606d3d98

rendering: window scaling throught initialMatrix
unconditionally baking the window scale into all shapes was not
statisfactory.
This patch sends the window scale through 'initialMatrix' on the GPU
render path only, and stores in CachedSurfaces the scale the shape was
rendered at. The shape is re-rendered later with a larger scale if
needed

src/scripting/flash/display/TokenContainer.cpp
@@ -295,25 +295,17 @@ IDrawable* TokenContainer::invalidate(DisplayObject* target, const MATRIX& initi
 	MATRIX totalMatrix;
 	std::vector<IDrawable::MaskData> masks;
 
-	float scalex;
-	float scaley;
-	int offx,offy;
-	owner->getSystemState()->stageCoordinateMapping(owner->getSystemState()->getRenderThread()->windowWidth,owner->getSystemState()->getRenderThread()->windowHeight,offx,offy, scalex,scaley);
-
 	bool isMask=false;
 	_NR<DisplayObject> mask;
 	if (target)
 	{
 		owner->computeMasksAndMatrix(target,masks,totalMatrix,false,isMask,mask);
-		totalMatrix=initialMatrix.multiplyMatrix(totalMatrix);
+		MATRIX initialNoRotation(initialMatrix.getScaleX(), initialMatrix.getScaleY(),
+				0, 0, initialMatrix.getTranslateX(), initialMatrix.getTranslateY());
+		totalMatrix=initialNoRotation.multiplyMatrix(totalMatrix);
 	}
 	owner->computeBoundsForTransformedRect(bxmin,bxmax,bymin,bymax,x,y,width,height,totalMatrix);
 
-	width = bxmax-bxmin;
-	height = bymax-bymin;
-	float rotation = owner->getConcatenatedMatrix().getRotation();
-	float xscale = owner->getConcatenatedMatrix().getScaleX();
-	float yscale = owner->getConcatenatedMatrix().getScaleY();
 	float redMultiplier=1.0;
 	float greenMultiplier=1.0;
 	float blueMultiplier=1.0;
@@ -362,9 +354,9 @@ IDrawable* TokenContainer::invalidate(DisplayObject* target, const MATRIX& initi
 	}
 	owner->cachedSurface.isValid=true;
 	return new CairoTokenRenderer(tokens,totalMatrix2
-				, x*scalex, y*scaley, ceil(width*scalex), ceil(height*scaley)
-				, rx*scalex, ry*scaley, ceil(rwidth*scalex), ceil(rheight*scaley), rotation
-				, xscale, yscale
+				, x, y, ceil(width), ceil(height)
+				, rx, ry, ceil(rwidth), ceil(rheight), totalMatrix2.getRotation()
+				, totalMatrix2.getScaleX(), totalMatrix2.getScaleY()
 				, isMask, mask
 				, scaling,owner->getConcatenatedAlpha(), masks
 				, redMultiplier,greenMultiplier,blueMultiplier,alphaMultiplier

src/backends/decoder.cpp
@@ -80,7 +80,7 @@ void VideoDecoder::sizeNeeded(uint32_t& w, uint32_t& h) const
 	h=frameHeight;
 }
 
-const TextureChunk& VideoDecoder::getTexture()
+TextureChunk& VideoDecoder::getTexture()
 {
 	return videoTexture;
 }

src/backends/decoder.h
@@ -126,7 +126,7 @@ public:
 	void waitForFencing();
 	//ITextureUploadable interface
 	void sizeNeeded(uint32_t& w, uint32_t& h) const;
-	const TextureChunk& getTexture();
+	TextureChunk& getTexture();
 	void uploadFence();
 	void markForDestruction();
 	bool isUploading() { return fenceCount; }

src/backends/graphics.cpp
@@ -46,6 +46,8 @@ TextureChunk::TextureChunk(uint32_t w, uint32_t h)
 	const uint32_t blocksW=(width+CHUNKSIZE_REAL-1)/CHUNKSIZE_REAL;
 	const uint32_t blocksH=(height+CHUNKSIZE_REAL-1)/CHUNKSIZE_REAL;
 	chunks=new uint32_t[blocksW*blocksH];
+	xContentScale = 1;
+	yContentScale = 1;
 }
 
 TextureChunk::TextureChunk(const TextureChunk& r):chunks(nullptr),texId(0),width(r.width),height(r.height)
@@ -74,6 +76,8 @@ TextureChunk& TextureChunk::operator=(const TextureChunk& r)
 	}
 	else
 		chunks=nullptr;
+	xContentScale = r.xContentScale;
+	yContentScale = r.yContentScale;
 	return *this;
 }
 
@@ -122,7 +126,7 @@ CairoRenderer::CairoRenderer(const MATRIX& _m, int32_t _x, int32_t _y, int32_t _
 		float _redMultiplier,float _greenMultiplier,float _blueMultiplier,float _alphaMultiplier,
 		float _redOffset,float _greenOffset,float _blueOffset,float _alphaOffset,
 		bool _smoothing)
-	: IDrawable(_w, _h, _x, _y, _rw, _rh, _rx, _ry, _r, _xs, _ys, _im, _mask,_a, _ms,
+	: IDrawable(_w, _h, _x, _y, _rw, _rh, _rx, _ry, _r, _xs, _ys, _xs, _ys, _im, _mask,_a, _ms,
 				_redMultiplier,_greenMultiplier,_blueMultiplier,_alphaMultiplier,
 				_redOffset,_greenOffset,_blueOffset,_alphaOffset,_smoothing,_m)
 	, scaleFactor(_s)
@@ -589,10 +593,16 @@ uint8_t* CairoRenderer::getPixelBuffer(bool *isBufferOwner, uint32_t* bufsize)
 		return nullptr;
 
 	uint8_t* ret=nullptr;
-
 	cairo_surface_t* cairoSurface=allocateSurface(ret);
-
 	cairo_t* cr=cairo_create(cairoSurface);
+
+	// scale: keep draws in positive quadrant
+	if (xscale < 0)
+		cairo_translate(cr, width, 0);
+	if (yscale < 0)
+		cairo_translate(cr, 0, height);
+	cairo_scale(cr, xscale, yscale);
+
 	cairo_surface_destroy(cairoSurface); /* cr has an reference to it */
 	cairoClean(cr);
 	cairo_set_antialias(cr,smoothing ? CAIRO_ANTIALIAS_DEFAULT : CAIRO_ANTIALIAS_NONE);
@@ -657,6 +667,16 @@ uint8_t* CairoRenderer::getPixelBuffer(bool *isBufferOwner, uint32_t* bufsize)
 //	cairo_surface_write_to_png(cairoSurface,"/tmp/cairo.png");
 	cairo_destroy(cr);
 	return ret;
+}
+
+bool CairoRenderer::isCachedSurfaceUsable(const DisplayObject* o) const
+{
+	const TextureChunk* tex = o->cachedSurface.tex;
+
+	// arbitrary regen threshold
+	return !tex || !tex->isValid() ||
+		(abs(xscale / tex->xContentScale) < 2
+		&& abs(yscale / tex->yContentScale) < 2);
 }
 
 bool CairoTokenRenderer::hitTest(const tokensVector& tokens, float scaleFactor, number_t x, number_t y)
@@ -1074,7 +1094,7 @@ void AsyncDrawJob::sizeNeeded(uint32_t& w, uint32_t& h) const
 	h=drawable->getHeight();
 }
 
-const TextureChunk& AsyncDrawJob::getTexture()
+TextureChunk& AsyncDrawJob::getTexture()
 {
 	/* This is called in the render thread,
 	 * so we need no locking for surface */
@@ -1126,6 +1146,12 @@ void AsyncDrawJob::uploadFence()
 	delete this;
 }
 
+void AsyncDrawJob::contentScale(float& x, float& y) const
+{
+	x = drawable->getXContentScale();
+	y = drawable->getYContentScale();
+}
+
 void SoftwareInvalidateQueue::addToInvalidateQueue(_R<DisplayObject> d)
 {
 	queue.emplace_back(d);
@@ -1146,7 +1172,7 @@ BitmapRenderer::BitmapRenderer(_NR<BitmapContainer> _data, int32_t _x, int32_t _
 		float _a, const std::vector<MaskData>& _ms,
 		float _redMultiplier, float _greenMultiplier, float _blueMultiplier, float _alphaMultiplier,
 		float _redOffset, float _greenOffset, float _blueOffset, float _alphaOffset, bool _smoothing, const MATRIX& _m)
-	: IDrawable(_w, _h, _x, _y, _rw, _rh, _rx, _ry, _r, _xs, _ys, _im, _mask,_a, _ms,
+	: IDrawable(_w, _h, _x, _y, _rw, _rh, _rx, _ry, _r, _xs, _ys, 1, 1, _im, _mask,_a, _ms,
 				_redMultiplier,_greenMultiplier,_blueMultiplier,_alphaMultiplier,
 				_redOffset,_greenOffset,_blueOffset,_alphaOffset,_smoothing,_m)
 	, data(_data)
@@ -1168,7 +1194,7 @@ void CharacterRenderer::upload(uint8_t *data, uint32_t w, uint32_t h)
 	memcpy(data, this->data, w*h*4);
 }
 
-const TextureChunk& CharacterRenderer::getTexture()
+TextureChunk& CharacterRenderer::getTexture()
 {
 	if(!chunk.resizeIfLargeEnough(width, height))
 	chunk=getSys()->getRenderThread()->allocateTexture(width, height,false);

src/backends/graphics.h
@@ -55,7 +55,7 @@ private:
 	uint32_t texId;
 	TextureChunk(uint32_t w, uint32_t h);
 public:
-	TextureChunk():chunks(nullptr),texId(0),width(0),height(0){}
+	TextureChunk():chunks(nullptr),texId(0),width(0),height(0),xContentScale(1),yContentScale(1) {}
 	TextureChunk(const TextureChunk& r);
 	TextureChunk& operator=(const TextureChunk& r);
 	~TextureChunk();
@@ -65,14 +65,16 @@ public:
 	void makeEmpty();
 	uint32_t width;
 	uint32_t height;
+	float xContentScale; // scale the bitmap content was generated for
+	float yContentScale;
 };
 
 class CachedSurface
 {
 public:
-	CachedSurface():tex(nullptr),xOffset(0),yOffset(0),xOffsetTransformed(0),yOffsetTransformed(0),widthTransformed(0),heightTransformed(0),alpha(1.0),rotation(0.0),xscale(1.0),yscale(1.0)
-	  , redMultiplier(1.0), greenMultiplier(1.0), blueMultiplier(1.0), alphaMultiplier(1.0), redOffset(0.0), greenOffset(0.0), blueOffset(0.0), alphaOffset(0.0)
-	  ,isMask(false),smoothing(true),isChunkOwner(true),isValid(false){}
+	CachedSurface():tex(nullptr),xOffset(0),yOffset(0),xOffsetTransformed(0),yOffsetTransformed(0),widthTransformed(0),heightTransformed(0),alpha(1.0),rotation(0.0),xscale(1.0),yscale(1.0),
+		redMultiplier(1.0), greenMultiplier(1.0), blueMultiplier(1.0), alphaMultiplier(1.0), redOffset(0.0), greenOffset(0.0), blueOffset(0.0), alphaOffset(0.0)
+		,isMask(false),smoothing(true),isChunkOwner(true),isValid(false){}
 	~CachedSurface()
 	{
 		if (isChunkOwner && tex)
@@ -112,11 +114,12 @@ protected:
 	~ITextureUploadable(){}
 public:
 	virtual void sizeNeeded(uint32_t& w, uint32_t& h) const=0;
+	virtual void contentScale(float& x, float& y) const {x = 1; y = 1;}
 	/*
 		Upload data to memory mapped to the graphics card (note: size is guaranteed to be enough
 	*/
 	virtual void upload(uint8_t* data, uint32_t w, uint32_t h)=0;
-	virtual const TextureChunk& getTexture()=0;
+	virtual TextureChunk& getTexture()=0;
 	/*
 		Signal the completion of the upload to the texture
 		NOTE: fence may be called on shutdown even if the upload has not happen, so be ready for this event
@@ -157,6 +160,8 @@ protected:
 	float alpha;
 	float xscale;
 	float yscale;
+	float xContentScale;
+	float yContentScale;
 	float redMultiplier;
 	float greenMultiplier;
 	float blueMultiplier;
@@ -175,14 +180,14 @@ protected:
 public:
 	IDrawable(int32_t w, int32_t h, int32_t x, int32_t y,
 		int32_t rw, int32_t rh, int32_t rx, int32_t ry, float r,
-		float xs, float ys,
+		float xs, float ys, float xcs, float ycs,
 		bool im, _NR<DisplayObject> _mask,
 		float a, const std::vector<MaskData>& m,
 		float _redMultiplier,float _greenMultiplier,float _blueMultiplier,float _alphaMultiplier,
 		float _redOffset,float _greenOffset,float _blueOffset,float _alphaOffset, bool _smoothing,
 		const MATRIX& _m):
 		masks(m),width(w),height(h),xOffset(x),yOffset(y),xOffsetTransformed(rx),yOffsetTransformed(ry),widthTransformed(rw),heightTransformed(rh),rotation(r),
-		alpha(a),xscale(xs),yscale(ys),
+		alpha(a), xscale(xs), yscale(ys), xContentScale(xcs), yContentScale(ycs),
 		redMultiplier(_redMultiplier),greenMultiplier(_greenMultiplier),blueMultiplier(_blueMultiplier),alphaMultiplier(_alphaMultiplier),
 		redOffset(_redOffset),greenOffset(_greenOffset),blueOffset(_blueOffset),alphaOffset(_alphaOffset),
 		isMask(im),mask(_mask),smoothing(_smoothing), matrix(_m) {}
@@ -198,6 +203,7 @@ public:
 	 * another object
 	 */
 	virtual void applyCairoMask(cairo_t* cr, int32_t offsetX, int32_t offsetY) const = 0;
+	virtual bool isCachedSurfaceUsable(const DisplayObject*) const {return true;}
 	int32_t getWidth() const { return width; }
 	int32_t getHeight() const { return height; }
 	int32_t getWidthTransformed() const { return widthTransformed; }
@@ -210,6 +216,8 @@ public:
 	float getAlpha() const { return alpha; }
 	float getXScale() const { return xscale; }
 	float getYScale() const { return yscale; }
+	float getXContentScale() const { return xContentScale; }
+	float getYContentScale() const { return yContentScale; }
 	bool getIsMask() const { return isMask; }
 	_NR<DisplayObject> getMask() const { return mask; }
 	bool getSmoothing() const { return smoothing; }
@@ -252,8 +260,9 @@ public:
 	//ITextureUploadable interface
 	void upload(uint8_t* data, uint32_t w, uint32_t h) override;
 	void sizeNeeded(uint32_t& w, uint32_t& h) const override;
-	const TextureChunk& getTexture() override;
+	TextureChunk& getTexture() override;
 	void uploadFence() override;
+	void contentScale(float& x, float& y) const override;
 	DisplayObject* getOwner() { return owner.getPtr(); }
 };
 
@@ -285,6 +294,7 @@ public:
 				  , bool _smoothing);
 	//IDrawable interface
 	uint8_t* getPixelBuffer(bool* isBufferOwner=nullptr, uint32_t* bufsize=nullptr) override;
+	bool isCachedSurfaceUsable(const DisplayObject*) const override;
 	/*
 	 * Converts data (which is in RGB format) to the format internally used by cairo.
 	 */
@@ -498,7 +508,7 @@ public:
 	//ITextureUploadable interface
 	void sizeNeeded(uint32_t& w, uint32_t& h) const override { w=width; h=height;}
 	void upload(uint8_t* data, uint32_t w, uint32_t h) override;
-	const TextureChunk& getTexture() override;
+	TextureChunk& getTexture() override;
 	void uploadFence() override {}
 };
 

src/backends/rendering.cpp
@@ -107,7 +107,8 @@ void RenderThread::finalizeUpload()
 	ITextureUploadable* u=prevUploadJob;
 	uint32_t w,h;
 	u->sizeNeeded(w,h);
-	const TextureChunk& tex=u->getTexture();
+	TextureChunk& tex=u->getTexture();
+	u->contentScale(tex.xContentScale, tex.yContentScale);
 	loadChunkBGRA(tex, w, h, engineData->getCurrentPixBuf());
 	u->uploadFence();
 	prevUploadJob=nullptr;

src/backends/rendering_context.cpp
@@ -178,7 +178,7 @@ void GLRenderContext::renderTextured(const TextureChunk& chunk, int32_t x, int32
 	engineData->exec_glUniform2f(beforeRotateUniform, float(w)/2.0,float(h)/2.0);
 	engineData->exec_glUniform2f(afterRotateUniform, float(widthtransformed)/2.0,float(heighttransformed)/2.0);
 	engineData->exec_glUniform2f(startPositionUniform, xtransformed,ytransformed);
-	engineData->exec_glUniform2f(scaleUniform, xscale,yscale);
+	engineData->exec_glUniform2f(scaleUniform, xscale / chunk.xContentScale, yscale / chunk.yContentScale);
 	engineData->exec_glUniform4f(colortransMultiplyUniform, redMultiplier,greenMultiplier,blueMultiplier,alphaMultiplier);
 	engineData->exec_glUniform4f(colortransAddUniform, redOffset/255.0,greenOffset/255.0,blueOffset/255.0,alphaOffset/255.0);
 	// set mode for direct coloring:
@@ -378,14 +378,21 @@ void CairoRenderContext::renderTextured(const TextureChunk& chunk, int32_t x, in
 	cairo_surface_t* chunkSurface = getCairoSurfaceForData(buf, chunk.width, chunk.height);
 	cairo_save(cr);
 	cairo_set_antialias(cr,smooth && !isMask ? CAIRO_ANTIALIAS_DEFAULT : CAIRO_ANTIALIAS_NONE);
+
+	// scale: keep draws in positive quadrant
 	cairo_set_matrix(cr,&matrix);
+	if (chunk.xContentScale < 0)
+		cairo_translate(cr, w / -chunk.xContentScale, 0);
+	if (chunk.yContentScale < 0)
+		cairo_translate(cr, 0, h / -chunk.yContentScale);
+	cairo_scale(cr, 1 / chunk.xContentScale, 1 / chunk.yContentScale);
 
 	if(isMask)
 	{
 		if (masksurface) // reset previous mask
 			cairo_surface_destroy(masksurface);
 		masksurface = chunkSurface;
-		maskmatrix=matrix;
+		cairo_get_matrix(cr, &maskmatrix);
 	}
 	cairo_set_source_surface(cr, chunkSurface, 0,0);
 	if (hasMask)

src/platforms/engineutils.cpp
@@ -1432,7 +1432,7 @@ externalFontRenderer::externalFontRenderer(const TextData &_textData, EngineData
 										   float _redMultiplier,float _greenMultiplier,float _blueMultiplier,float _alphaMultiplier,
 										   float _redOffset,float _greenOffset,float _blueOffset,float _alphaOffset,
 										   bool smoothing, const MATRIX &_m)
-	: IDrawable(w, h, x, y,rw,rh,rx,ry,r,xs,ys,im,_mask, a, m,
+	: IDrawable(w, h, x, y,rw,rh,rx,ry,r,xs,ys, 1, 1, im, _mask, a, m,
 				_redMultiplier,_greenMultiplier,_blueMultiplier,_alphaMultiplier,
 				_redOffset,_greenOffset,_blueOffset,_alphaOffset,smoothing,_m),m_engine(engine)
 {

commit 40c967cd577381a9aa6fef6f3670c7e431b8c5aa

graphics: fix regression regarding 0-scale draws
introduced in f7964bf. Fixes #886

src/scripting/flash/display/TokenContainer.cpp
@@ -306,6 +306,13 @@ IDrawable* TokenContainer::invalidate(DisplayObject* target, const MATRIX& initi
 	}
 	owner->computeBoundsForTransformedRect(bxmin,bxmax,bymin,bymax,x,y,width,height,totalMatrix);
 
+	if (isnan(width) || isnan(height))
+	{
+		// on stage with invalid contatenatedMatrix. Create a trash initial texture
+		width = 1;
+		height = 1;
+	}
+
 	float redMultiplier=1.0;
 	float greenMultiplier=1.0;
 	float blueMultiplier=1.0;
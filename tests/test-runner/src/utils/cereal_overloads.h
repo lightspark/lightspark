dae6666c3c6b1771755d4f18a1973beb1f07e6a2
mrb0nk500 <b0nk@b0nk.xyz>
2024-03-29 17:25:02 -0300

    [test-runner] Fix some errors, and overlooked stuff

--- tests/test-runner/src/utils/cereal_overloads.h
+++ tests/test-runner/src/utils/cereal_overloads.h
@@ -1,5 +1,24 @@
-#ifndef CEREAL_OVERLOADS_H
-#define CEREAL_OVERLOADS_H
+/**************************************************************************
+    Lightspark, a free flash player implementation
+
+    Copyright (C) 2024  mr b0nk 500 (b0nk@b0nk.xyz)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#ifndef UTILS_CEREAL_OVERLOADS_H
+#define UTILS_CEREAL_OVERLOADS_H 1
 
 #include <cstdint>
 #include <utility>
@@ -102,4 +121,4 @@ void prologue(Archive &, T &) {}
 template<class Archive, class T>
 void epilogue(Archive &, T &) {}
 
-#endif
+#endif /* UTILS_CEREAL_OVERLOADS_H */

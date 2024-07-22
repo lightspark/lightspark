ef2ae5811c54877ed882d6865b5628296b8ee172
Paulo Medeiros <paulo.medeiros1302@gmail.com>
2024-07-22 17:00:45 +0000

    Add Missing Changes

--- src/allclasses.h
+++ src/allclasses.h
@@ -130,6 +130,7 @@ REGISTER_CLASS_NAME(StageQuality,"flash.display")
 REGISTER_CLASS_NAME(SWFVersion,"flash.display")
 REGISTER_CLASS_NAME(TriangleCulling,"flash.display")
 REGISTER_CLASS_NAME(NativeWindow,"flash.display")
+REGISTER_CLASS_NAME(StageAspectRatio,"flash.display")
 
 //Display3D
 REGISTER_CLASS_NAME(Context3D,"flash.display3D")

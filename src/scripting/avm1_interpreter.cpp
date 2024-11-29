0c1e5dba8584237f12188dbad82ff3b80d65787b
mrb0nk500 <b0nk@b0nk.xyz>
2024-11-29 17:50:24 -0400

    [AVM1] Clamp argument count to the stack size for `ActionNewMethod`
    This fixes a crash with Ruffle's `avm1/new_method_wrap` test.

--- src/scripting/avm1_interpreter.cpp
+++ src/scripting/avm1_interpreter.cpp
@@ -1894,9 +1894,9 @@ void ACTIONRECORD::executeActions(DisplayObject *clip, AVM1context* context, con
 				asAtom name = PopStack(stack);
 				asAtom scriptobject = PopStack(stack);
 				asAtom na = PopStack(stack);
-				uint32_t numargs = asAtomHandler::toUInt(na);
+				size_t numargs = std::min((size_t)asAtomHandler::toUInt(na), stack.size());
 				asAtom* args = g_newa(asAtom, numargs);
-				for (uint32_t i = 0; i < numargs; i++)
+				for (size_t i = 0; i < numargs; i++)
 					args[i] = PopStack(stack);
 				LOG_CALL("AVM1:"<<clip->getTagID()<<" "<<(clip->is<MovieClip>() ? clip->as<MovieClip>()->state.FP : 0)<<" ActionNewMethod "<<asAtomHandler::toDebugString(name)<<" "<<numargs<<" "<<asAtomHandler::toDebugString(scriptobject));
 				uint32_t nameID = asAtomHandler::toStringId(name,wrk);

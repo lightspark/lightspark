b3a1addd7f192757849ef577ace87dc46518b854
mrb0nk500 <b0nk@b0nk.xyz>
2024-12-24 13:06:07 -0400

    [compat] Use `CreateWaitableTimerExW()` instead

--- src/compat.cpp
+++ src/compat.cpp
@@ -32,7 +32,7 @@
 #	define WIN32_LEAN_AND_MEAN
 #	include <windows.h>
 #	include <winnt.h> // for Nt{Set,Query}TimerResolution()
-#	include <winbase.h> // for `CreateWaitableTimerEx()`
+#	include <winbase.h> // for `CreateWaitableTimerExW()`
 #	include <versionhelpers.h> // for `IsWindowsVistaOrGreater()`
 #	undef DOUBLE_CLICK
 #	undef RGB
@@ -183,7 +183,7 @@ void compat_usleep(uint64_t us)
 	period.QuadPart = -us*10;
 	HANDLE timer;
 	if (IsWindowsVistaOrGreater())
-		timer = CreateWaitableTimerEx(NULL, NULL, 2, TIMER_ALL_ACCESS);
+		timer = CreateWaitableTimerExW(NULL, NULL, 2, TIMER_ALL_ACCESS);
 	else
 		timer = CreateWaitableTimer(NULL, false, NULL);
 	SetWaitableTimer(timer, &period, 0, NULL, NULL, false);

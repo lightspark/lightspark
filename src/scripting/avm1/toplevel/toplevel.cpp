bb1e5f6570b13e5f215b82b2c43fca3f07d905ed
mrb0nk500 <b0nk@b0nk.xyz>
2025-10-15 17:51:02 -0300

    [AVM1/toplevel] Implement a few more toplevel/global functions
    Still a few left.

--- src/scripting/avm1/toplevel/toplevel.cpp
+++ src/scripting/avm1/toplevel/toplevel.cpp
@@ -37,13 +37,174 @@ using namespace lightspark;
 
 // Based on Ruffle's `avm1::globals` crate.
 
+AVM1Value parseIntImpl
+(
+	AVM1Activation& act,
+	const tiny_string& str,
+	const Optional<int32_t>& radix
+)
+{
+	constexpr auto NaN = std::numeric_limits<number_t>::quiet_NaN();
+
+	if (radix.hasValue() && (*radix < 2 || *radix > 36))
+		return NaN;
+
+	auto parseSign = [&](CharIterator& it) -> Optional<number_t>
+	{
+		if (*it != '+' && *it != '-')
+			return {};
+		return *it++ == '+' ? 1 : -1;
+	};
+
+	auto isOctalDigit = [](uint32_t ch)
+	{
+		return ch >= '0' && ch <= '7';
+	};
+
+	int32_t _radix = radix.valueOr(10);
+	bool ignoreSign = false;
+
+	auto it = str.begin();
+	bool hasSign = parseSign(it).hasValue();
+	bool isZero = it != str.end() && *it++ == '0';
+	bool isHex = isZero && (*it == 'x' || *it == 'X');
+
+	// NOTE, FLASH-PLAYER-BUG: Unless the hex prefix (`0x`) is a valid
+	// digit sequence in the given radix, the prefixes `[+-]0{x,X}`
+	// return `NaN`, instead of 0.
+	// Otherwise the sign should be ignored.
+	if (isHex && hasSign && radix.valueOr(0) < 34)
+		return NaN;
+	else if (isHex && hasSign)
+	{
+		_radix = *radix;
+		ignoreSign = true;
+		it = str.begin();
+	}
+	else if (isHex)
+	{
+		// Auto detect a hexadecimal prefix (`0x`), and strip it.
+		// NOTE, FLASH-PLAYER-BUG: The prefix is always stripped,
+		// regardless of the radix.
+		// `parseInt("0x100", 10) == 100`, instead of 0.
+		// `parseInt("0x100", 36) == 1296`, instead of 1540944.
+
+		// NOTE, FLASH-PLAYER-BUG: The prefix is expected before the sign,
+		// or any whitespace.
+		// `parseInt("0x -10") == -16`, instead of `NaN`.
+		// `parseInt(" -0x10") == NaN`, instead of -16.
+		_radix = radix.valueOr(16);
+		++it;
+	}
+	else if (isZero && std::all_of(it, str.end(), isOctalDigit))
+	{
+		// NOTE, ECMA-262 violation: Octal prefixes (`[+-]0`).
+		// NOTE: A number with an octal prefix can't contain leading
+		// whitespace, or extra trailing characters.
+		_radix = 8;
+		it = str.begin();
+	}
+
+	// Strip any leading whitespace.
+	it = std::find_if(it, str.end(), [](uint32_t ch)
+	{
+		return
+		(
+			ch != '\t' &&
+			ch != '\n' &&
+			ch != '\r' &&
+			ch != ' '
+		);
+	});
+
+
+	// Based on Rust's `char::to_digit()`.
+	auto toDigit = [&](uint32_t ch, int32_t radix) -> Optional<int32_t>
+	{
+		if (radix < 2 || radix > 36)
+			return {};
+
+		int32_t value =
+		(
+			ch > '9' && radix > 10 ?
+			// Convert an ASCII letter into it's corresponding value
+			// (`[a-zA-Z]` -> `10-35`).
+			// NOTE: Other characters will produce values >= 36.
+			//
+			// NOTE: The subtraction is done inside the `toupper()` call
+			// to prevent the value from exceeding `UINT32_MAX - 0x20`.
+			toupper(ch - 'A') + 10 :
+			// Convert the digit to a value.
+			// NOTE: Non digit characters will produce values > 36.
+			ch - '0'
+		);
+
+		if (value >= radix)
+			return {};
+		return value;
+	};
+
+	auto sign = parseSign(it).filter(ignoreSign).valueOr(1);
+
+	bool empty = true;
+	number_t ret = 0;
+
+	for (; it != str.end(); ++it)
+	{
+		auto digit = toDigit(*it, _radix);
+		if (!digit.hasValue())
+			break;
+		ret = ret * _radix + *digit;
+		empty = false;
+	}
+
+	return empty ? NaN : std::copysign(ret, sign);
+}
+
+template<bool isTimeout>
+AVM1_FUNCTION_DECL(createTimer)
+{
+	// NOTE: `setInterval()` was added in Flash Player 6, but isn't
+	// version gated.
+	AVM1_ARG_UNPACK_NAMED(unpacker);
+
+	GcPtr<AVM1Object> obj = _this;
+	AVM1_ARG_CHECK(unpacker(obj));
+
+	bool isFunc = obj->is<AVM1Executable>();
+
+	int32_t ival;
+	std::vector<AVM1Value> funcArgs;
+	AVM1_ARG_CHECK(unpacker.next(!isFunc)(ival, [&](const auto& val)
+	{
+		return !val.is<UndefinedVal>();
+	})(funcArgs).prev(!isFunc));
+
+	Impl<IIntervalRunner> callback;
+	if (isFunc)
+		callback = AVM1IntervalRunner(obj, funcArgs);
+	else
+	{
+		tiny_string methodName;
+		unpacker(methodName);
+		callback = AVM1IntervalRunner(obj, methodName, funcArgs);
+	}
+
+	return number_t(act.getSys()->intervalManager->addTimer
+	(
+		callback,
+		ival,
+		isTimeout
+	));
+}
+
 AVM1_FUNCTION_DECL(trace)
 {
 	// NOTE: Unlike `ActionTrace`, `_global.trace()` always converts
 	// `undefined` to `""` in SWF 6, and earlier. It also doesn't log
 	// anything outside of Flash's (the authoring tool) trace window.
-	const auto& undefVal = AVM1Value::undefinedVal;
-	auto str = (args.empty() ? undefVal : args[0]).toString(act);
+	tiny_string str;
+	AVM1_ARG_UNPACK(str);
 	act.getSys()->trace(str);
 	return undefVal;
 }
@@ -60,20 +221,21 @@ AVM1_FUNCTION_DECL(isNaN)
 
 AVM1_FUNCTION_DECL(parseInt)
 {
-	// NOTE: ECMA-262 says `parseInt()` returns `NaN`, but AVM1 returns
-	// `undefined` instead.
-	if (args.empty())
-		return AVM1Value::undefinedVal;
-
 	AVM1_ARG_UNPACK_NAMED(unpacker);
-	return parseIntImpl(act, unpacker[0], unpacker.tryGetArg(1))
+
+	// NOTE, ECMA-262 violation: `parseInt()` returns `undefined`, instead
+	// of `NaN`.
+	tiny_string str;
+	Optional<int32_t> radix
+	AVM1_ARG_CHECK(unpacker(str)(radix));
+	return parseIntImpl(act, str, radix)
 }
 
 AVM1_FUNCTION_DECL(parseFloat)
 {
-	if (args.empty())
-		return AVM1Value::undefinedVal;
-	return parseFloatImpl(args[0].toString(act), false);
+	tiny_string str;
+	AVM1_ARG_CHECK(AVM1_ARG_UNPACK(str));
+	return parseFloatImpl(str, false);
 }
 
 // This is an undocumented function that allows AS 2 classes to set the
@@ -115,12 +277,9 @@ AVM1_FUNCTION_DECL(asSetPropFlags)
 
 AVM1_FUNCTION_DECL(clearInterval)
 {
-	const auto& undefVal = AVM1Value::undefinedVal;
-	act.getSys()->intervalManager->clearInterval
-	(
-		(args.empty() ? undefVal : args[0]).toInt32(act),
-		false
-	);
+	int32_t id;
+	AVM1_ARG_UNPACK(id);
+	act.getSys()->intervalManager->clearInterval(id, false);
 	return AVM1Value::undefinedVal;
 }
 
@@ -131,12 +290,9 @@ AVM1_FUNCTION_DECL(setInterval)
 
 AVM1_FUNCTION_DECL(clearTimeout)
 {
-	const auto& undefVal = AVM1Value::undefinedVal;
-	act.getSys()->intervalManager->clearInterval
-	(
-		(args.empty() ? undefVal : args[0]).toInt32(act),
-		true
-	);
+	int32_t id;
+	AVM1_ARG_UNPACK(id);
+	act.getSys()->intervalManager->clearInterval(id, true);
 	return AVM1Value::undefinedVal;
 }
 
@@ -153,18 +309,16 @@ AVM1_FUNCTION_DECL(updateAfterEvent)
 
 AVM1_FUNCTION_DECL(escape)
 {
-	if (args.empty())
-		return AVM1Value::undefinedVal;
-
-	return URLInfo::encode(args[0].toString(act), URLInfo::ENCODE_ESCAPE);
+	tiny_string str;
+	AVM1_ARG_CHECK(AVM1_ARG_UNPACK(str));
+	return URLInfo::encode(str, URLInfo::ENCODE_ESCAPE);
 }
 
 AVM1_FUNCTION_DECL(unescape)
 {
-	if (args.empty())
-		return AVM1Value::undefinedVal;
-
-	return URLInfo::decode(args[0].toString(act), URLInfo::ENCODE_ESCAPE);
+	tiny_string str;
+	AVM1_ARG_CHECK(AVM1_ARG_UNPACK(str));
+	return URLInfo::decode(str, URLInfo::ENCODE_ESCAPE);
 }
 
 AVM1_FUNCTION_DECL(getNaN)

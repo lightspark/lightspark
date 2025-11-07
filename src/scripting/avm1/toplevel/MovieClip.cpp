611e4bb50484fe9e9de9d3caeef929201aff9b79
mrb0nk500 <b0nk@b0nk.xyz>
2025-11-07 17:29:53 -0400

    [AVM1MovieClip] Implement some more functions
    Still have way more left to do.

--- src/scripting/avm1/toplevel/MovieClip.cpp
+++ src/scripting/avm1/toplevel/MovieClip.cpp
@@ -32,7 +32,8 @@
 #include "scripting/avm1/prop.h"
 #include "scripting/avm1/prop_decl.h"
 #include "scripting/avm1/runtime.h"
-#include "scripting/avm1/value.h"
+#include "scripting/avm1/toplevel/Matrix.h"
+#include "scripting/avm1/toplevel/MovieClip.h"
 #include "tiny_string.h"
 #include "utils/array.h"
 #include "utils/optional.h"
@@ -460,40 +461,395 @@ AVM1_MOVIECLIP_FUNC_BODY(lineStyle)
 	return AVM1Value::undefinedVal;
 }
 
+static Optional<std::vector<GRADRECORD>> buildGradRecords
+(
+	AVM1Activation& act,
+	const tiny_string& funcName,
+	const GcPtr<AVM1Object>& colors,
+	const GcPtr<AVM1Object>& alphas,
+	const GcPtr<AVM1Object>& ratios
+)
+{
+	auto numColors = colors->getLength(act);
+	auto numAlphas = alphas->getLength(act);
+	auto numRatios = ratios->getLength(act);
+	if (numColors != numAlphas || numColors != numRatios)
+	{
+		LOG
+		(
+			LOG_ERROR,
+			funcName << ": Received different size arrays for `colors`, "
+			"`alphas`, and `ratios`."
+		);
+		return nullOpt;
+	}
+
+	std::vector<GRADRECORD> records;
+	records.reserve(numColors);
+
+	for (size_t i = 0; i < numColors; ++i)
+	{
+		GRADRECORD record(0xff);
+		auto color = colors->getElement(act, i).toUInt32(act);
+		auto ratio = ratios->getElement(act, i).toNumber(act);
+		auto alpha = dclamp
+		(
+			alphas->getElement(act, i).toNumber(act),
+			0,
+			100
+		);
+
+		if (ratio <= 0 || ratio >= 256)
+		{
+			LOG
+			(
+				LOG_ERROR,
+				funcName << ": Received an invalid ratio value " << ratio
+			);
+			return nullOpt;
+		}
+
+		record.Color = RGBA(color, uint8_t(alpha / 100.0 * 255.0));
+		record.Ratio = ratio;
+		records.push_back(record);
+	}
+	return records;
+}
+
+static Optional<FILL_STYLE_TYPE> parseGradType(const tiny_string& type)
+{
+	if (type == "linear")
+		return LINEAR_GRADIENT;
+	else if (type == "radial")
+		return RADIAL_GRADIENT;
+	return {};
+}
+
+static GradientSpreadMode parseSpreadMethod(const tiny_string& method)
+{
+	if (type == "reflect")
+		return GradientSpreadMode::Reflect;
+	else if (type == "repeat")
+		return GradientSpreadMode::Repeat;
+	return GradientSpreadMode::Pad;
+}
+
+static GradientInterpMethod parseInterpMethod(const tiny_string& method)
+{
+	if (type == "linearRGB")
+		return GradientInterpMethod::LinearRGB;
+	return GradientInterpMethod::RGB;
+}
+
 AVM1_MOVIECLIP_FUNC_BODY(lineGradientStyle)
 {
+	AVM1_ARG_UNPACK_NAMED(unpacker);
+
+	tiny_string type;
+	AVM1Value colorsVal;
+	AVM1Value alphasVal;
+	AVM1Value ratiosVal;
+	AVM1Value matrixVal;
+	AVM1_ARG_CHECK(unpacker(type)(colorsVal)(alphasVal)(ratiosVal)(matrixVal));
+
+	// Silently fail, if too many arguments are passed.
+	if (args.size() > 8)
+		return AVM1Value::undefinedVal;
+
+	auto gradType = parseGradType(type);
+	if (!gradType.hasValue())
+	{
+		LOG(LOG_ERROR, "lineGradientStyle(): Received invalid fill type " << type);
+		return AVM1Value::undefinedVal;
+	}
+
+	auto records = buildGradRecords
+	(
+		act,
+		"lineGradientStyle()",
+		colorsVal.toObject(act),
+		alphasVal.toObject(act),
+		ratiosVal.toObject(act),
+	);
+
+	if (!records.hasValue())
+		return AVM1Value::undefinedVal;
+
+	auto matrix = AVM1Matrix::gradObjToMatrix
+	(
+		matrixVal.toObject(act)
+	);
+
+	tiny_string spreadMethod;
+	tiny_string interpMethod;
+	number_t focalPoint;
+	unpack(spreadMethod, "")(interpMethod, "")(focalPoint, 0);
+
+	auto spread = parseSpreadMethod(spreadMethod);
+	auto interp = parseInterpMethod(interpMethod);
+
+	#if 1
+	_this->getGraphics()->lineGradientStyle
+	(
+		*gradType,
+		records,
+		matrix,
+		spread,
+		interp,
+		focalPoint
+	);
+	#else
+	auto graphics = _this->getGraphics();
+
+	LINESTYLE2 style(0xff);
+	style.Width = graphics->getCurrentLineWidth();
+	style.HasFillFlag = true;
+
+	auto& fillStyle = style.FillType;
+	fillStyle.FillStyleType = *gradType;
+	fillStyle.Matrix = matrix;
+
+	auto& grad = fillStyle.Gradient;
+	grad.FocalPoint = FIXED8(focalPoint * 16384.0);
+	grad.GradientRecords = *records;
+	grad.SpreadMethod = spread;
+	grad.InterpolationModeMethod = interp;
+
+	graphics->AddStrokeToken(SET_STROKE);
+	graphics->AddLineStyleToken(graphics->addLineStyle(style));
+	#endif
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(beginFill)
 {
+	if (args.empty())
+		return endFill(act, _this, args);
+
+	auto graphics = _this->getGraphics();
+	graphics->dorender(true);
+
+	uint32_t color;
+	number_t alpha;
+	AVM1_ARG_CHECK(AVM1_ARG_UNPACK(color)(alpha, 100.0));
+
+	#if 1
+	_this->getGraphics()->beginFill(color, alpha / 100.0);
+	#else
+	graphics->setInFilling(true);
+
+	auto style = Graphics::createSolidFill(color, uint8_t
+	(
+		fclamp(alpha, 0, 100) /
+		100.0 *
+		255.0
+	));
+	graphics->AddFillToken(SET_FILL);
+	graphics->AddFillStyleToken(graphics->AddFillStyle(style));
+	#endif
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(beginBitmapFill)
 {
+	const auto& undefVal = AVM1Value::undefinedVal;
+	AVM1_ARG_UNPACK_NAMED(unpacker);
+
+	NullableGcPtr<AVM1BitmapData> data;
+	AVM1_ARG_CHECK_RET(unpacker(data), endFill(act, _this, args));
+
+	if (data.isNull())
+		return endFill(act, _this, args);
+
+	AVM1Value matrixVal;
+	bool repeat;
+	bool smoothing;
+	unpacker(matrixVal, undefVal)(repeat, true)(smoothing, false);
+
+	auto matrix = AVM1Matrix::objToMatrixOrIdent(act, matrixVal);
+
+	#if 1
+	_this->getGraphics()->beginBitmapFill
+	(
+		data,
+		matrix,
+		repeat,
+		smoothing
+	);
+	#else
+	auto graphics = _this->getGraphics();
+	graphics->dorender(true);
+	graphics->setInFilling(true);
+
+	auto style = Graphics::createBitmapFill
+	(
+		data,
+		matrix,
+		repeat,
+		smoothing
+	);
+	graphics->AddFillToken(SET_FILL);
+	graphics->AddFillStyleToken(graphics->AddFillStyle(style));
+	#endif
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(beginGradientFill)
 {
+	const auto& undefVal = AVM1Value::undefinedVal;
+	AVM1_ARG_UNPACK_NAMED(unpacker);
+
+	if (unpacker[0].valueOr(undefVal).is<UndefinedVal>())
+	{
+		// The path has no fill style, if the first argument is either
+		// `undefined`, or no arguments were passed.
+		//
+		// NOTE: `endFill()` is called here to remove the fill style from
+		// the path.
+		return endFill(act, _this, args);
+	}
+
+	tiny_string type;
+	AVM1Value colorsVal;
+	AVM1Value alphasVal;
+	AVM1Value ratiosVal;
+	AVM1Value matrixVal;
+
+
+	AVM1_ARG_CHECK(unpacker(type)(colorsVal)(alphasVal)(ratiosVal)(matrixVal));
+
+	// Silently fail, if too many arguments are passed.
+	if (args.size() > 8 || (args.size() > 5 && act.getSwfVersion() < 8))
+		return AVM1Value::undefinedVal;
+
+	auto gradType = parseGradType(type);
+	if (!gradType.hasValue())
+	{
+		LOG(LOG_ERROR, "beginGradientFill(): Received invalid fill type " << type);
+		return AVM1Value::undefinedVal;
+	}
+
+	auto records = buildGradRecords
+	(
+		act,
+		"beginGradientFill()",
+		colorsVal.toObject(act),
+		alphasVal.toObject(act),
+		ratiosVal.toObject(act),
+	);
+
+	if (!records.hasValue())
+		return AVM1Value::undefinedVal;
+
+	auto matrix = AVM1Matrix::gradObjToMatrix
+	(
+		matrixVal.toObject(act)
+	);
+
+	tiny_string spreadMethod;
+	tiny_string interpMethod;
+	number_t focalPoint;
+	unpack(spreadMethod, "")(interpMethod, "")(focalPoint, 0);
+
+	auto spread = parseSpreadMethod(spreadMethod);
+	auto interp = parseInterpMethod(interpMethod);
+
+	_this->getGraphics()->begiGradientFill
+	(
+		*gradType,
+		records,
+		matrix,
+		spread,
+		interp,
+		focalPoint
+	);
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(moveTo)
 {
+	if (args.size() < 2)
+		return AVM1Value::undefinedVal;
+
+	Vector2f pos;
+	AVM1_ARG_UNPACK(pos);
+
+	_this->getGraphics()->moveTo(pos);
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(lineTo)
 {
+	if (args.size() < 2)
+		return AVM1Value::undefinedVal;
+
+	Vector2f pos;
+	AVM1_ARG_UNPACK(pos);
+
+	_this->getGraphics()->lineTo(pos);
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(curveTo)
 {
+	if (args.size() < 4)
+		return AVM1Value::undefinedVal;
+
+	Vector2f control;
+	Vector2f anchor;
+	AVM1_ARG_UNPACK(control)(anchor);
+
+	#if 1
+	_this->getGraphics()->curveTo(control, anchor);
+	#else
+	auto graphics = _this->getGraphics();
+	graphics->updateTokenBounds(control);
+	graphics->updateTokenBounds(anchor);
+
+	if (graphics->isFilling())
+	{
+		graphic->AddFillToken(CURVE_QUADRATIC);
+		graphic->AddFillToken(control);
+		graphic->AddFillToken(anchor);
+	}
+
+	if (graphics->hasLineStyle())
+	{
+		graphic->AddStrokeToken(CURVE_QUADRATIC);
+		graphic->AddStrokeToken(control);
+		graphic->AddStrokeToken(anchor);
+	}
+
+	graphics->setHasChanged(true);
+	if (graphics->isFilling())
+		graphics->dorender(false);
+	#endif
+	return AVM1Value::undefinedVal;
+}
+
+AVM1_MOVIECLIP_FUNC_BODY(curveTo)
+{
+	if (args.size() < 4)
+		return AVM1Value::undefinedVal;
+
+	Vector2f control;
+	Vector2f anchor;
+	AVM1_ARG_UNPACK(control)(anchor);
+
+	_this->getGraphics()->curveTo(control, anchor);
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(endFill)
 {
+	_this->getGraphics()->endFill();
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(clear)
 {
+	_this->getGraphics()->clear();
+	return AVM1Value::undefinedVal;
 }
 
 AVM1_MOVIECLIP_FUNC_BODY(attachMovie)

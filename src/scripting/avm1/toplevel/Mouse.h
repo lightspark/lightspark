a0af7d6acd592dacd977a7adb4fc138bba0ef9a5
mrb0nk500 <b0nk@b0nk.xyz>
2025-11-23 15:37:10 -0400

    [AVM1] Implement the new version of `AVM1Mouse`

---
+++ src/scripting/avm1/toplevel/Mouse.h
@@ -0,0 +1,50 @@
+/**************************************************************************
+    Lightspark, a free flash player implementation
+
+    Copyright (C) 2025  mr b0nk 500 (b0nk@b0nk.xyz)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#ifndef SCRIPTING_AVM1_TOPLEVEL_MOUSE_H
+#define SCRIPTING_AVM1_TOPLEVEL_MOUSE_H 1
+
+#include "gc/ptr.h"
+#include "scripting/avm1/function.h"
+#include "scripting/avm1/object/object.h"
+
+// Based on Ruffle's `avm1::globals::mouse` crate.
+
+namespace lightspark
+{
+
+class AVM1DeclContext;
+struct AsBroadcasterFuncs;
+
+class AVM1Mouse : public AVM1Object
+{
+public:
+	AVM1Mouse
+	(
+		AVM1DeclContext& ctx,
+		const GcPtr<AsBroadcasterFuncs>& bcastFuncs,
+		const GcPtr<AVM1Object>& arrayProto
+	);
+
+	AVM1_FUNCTION_DECL(show);
+	AVM1_FUNCTION_DECL(hide);
+};
+
+}
+#endif /* SCRIPTING_AVM1_TOPLEVEL_MOUSE_H */

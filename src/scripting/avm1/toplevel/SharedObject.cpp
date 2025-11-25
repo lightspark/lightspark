75ba8c84db912fbbc467477592aea193a706939b
mrb0nk500 <b0nk@b0nk.xyz>
2025-11-25 17:04:12 -0400

    [AVM1SharedObject] Implement `getLocal()`
    This marks off `AVM1SharedObject` as done, for now.
    
    We can decide whether to support remote shared objects, or not, later.

--- src/scripting/avm1/toplevel/SharedObject.cpp
+++ src/scripting/avm1/toplevel/SharedObject.cpp
@@ -19,6 +19,7 @@
 
 #include <initializer_list>
 
+#include "backends/urlutils.h"
 #include "gc/context.h"
 #include "parsing/amf0_generator.h"
 #include "parsing/lso.h"
@@ -421,6 +422,187 @@ AVM1_FUNCTION_TYPE_BODY(AVM1SharedObject, AVM1SharedObject, getDiskUsage)
 
 AVM1_FUNCTION_BODY(AVM1SharedObject, getLocal)
 {
+	AVM1_ARG_UNPACK_NAMED(unpacker);
+
+	tiny_string name;
+	AVM1Value localPathVal;
+	// TODO: Flash Player seems to do some kind of escaping on `name`:
+	// e.g. "foo\uD800" becomes "fooE#FB#FB#D.sol".
+	unpacker(name)(localPathVal);
+
+	if (name.findFirst("~%&\\;:\"',<>?# ") != tiny_string::npos)
+	{
+		LOG
+		(
+			LOG_ERROR,
+			"SharedObject.getLocal(): Invalid character in `name`."
+		);
+		return AVM1Value::nullVal;
+	}
+
+	URLInfo movieURL(act.getBaseClip()->getMovie()->getURL());
+	if (!movieURL.isValid())
+	{
+		LOG
+		(
+			LOG_ERROR,
+			"SharedObject.getLocal(): Failed to parse the movie URL."
+		);
+		return AVM1Value::nullVal;
+	}
+
+	movieURL.setQuery("");
+	movieURL.setFragment("");
+
+	bool secure;
+	unpacker(secure, false);
+
+	// The `secure` argument disallows using the `SharedObject` from non
+	// HTTPS origins.
+	if (secure && movieURL.getProtocol() != "https")
+	{
+		LOG
+		(
+			LOG_ERROR,
+			"SharedObject.getLocal(): Tried to load a secure "
+			"`SharedObject` from a non HTTPS origin."
+		);
+		return AVM1Value::nullVal;
+	}
+
+	// NOTE: `SharedObject`s are sandboxed per domain.
+	// By default, they're keyed based on the SWF URL, but `localPath`
+	// can modify this path.
+	auto moviePath = movieURL.getPath();
+
+	// Remove any leading/trailing `/`s.
+	moviePath = moviePath.stripPrefix('/').stripSuffix('/');
+
+	auto movieHost = movieURL.getProtocol() == "file" ? [&]
+	{
+		// Remove any root prefixes from the path.
+		moviePath = Path(moviePath).getRelative().getGenericStr();
+		return "localhost";
+	}() : movieURL.getHostname();
+
+	auto localPath = localPathVal.tryAs
+	<
+		tiny_string
+	>().transformOr(moviePath, [&](const auto& _localPath)
+	{
+		// An empty `localPath` always fails.
+		if (_localPath.empty())
+			return "";
+
+		// Remove any leading/trailing `/`s.
+		auto localPath = _localPath.stripPrefix('/').stripSuffix('/');
+
+		// Verify that `localPath` is a prefix of the SWF path.
+		bool isValid = moviePath.startsWith(localPath) &&
+		(
+			localPath.empty() ||
+			moviePath.numChars() == localPath.numChars() ||
+			moviePath.stripPrefix(localPath).startsWith('/')
+		);
+
+		if (isValid)
+			return localPath;
+
+		LOG
+		(
+			LOG_ERROR,
+			"SharedObject.getLocal(): `localPath` doesn't match the SWF "
+			"path."
+		);
+		return "";
+	});
+
+	if (localPath.empty())
+		return AVM1Value::nullVal;
+
+	// Create the final path: `foo.com/dir/movie.swf/name`.
+	// NOTE: In Flash Player, if `name` is a path containing `/`s, then
+	// it's prefixed with a `#`.
+	tiny_string prefix = path.contains('/') ? "#" : "";
+	auto fullPath = Path(moviePath) / localPath / prefix + name;
+
+	// Don't allow paths with `..` in them, to prevent an SWF from
+	// traversing the file system.
+	bool hasParentDir = std::any_of
+	(
+		fullPath.begin()
+		fullPath.end(),
+		[&](const auto& path) { return path == ".."; }
+	);
+
+	if (hasParentDir)
+	{
+		LOG
+		(
+			LOG_ERROR,
+			"SharedObject.getLocal(): Invalid path with `..` segments."
+		);
+		return AVM1Value::nullVal;
+	}
+
+	// NOTE: Flash Player usually fails to save `SharedObject`s that use
+	// `.` files/directories, so, disallow them entirely.
+	bool hasDotPath = std::any_of
+	(
+		fullPath.begin()
+		fullPath.end(),
+		[&](const auto& path) { return path.getStr().startsWith('.'); }
+	);
+
+	if (hasDotPath)
+	{
+		LOG
+		(
+			LOG_ERROR,
+			"SharedObject.getLocal(): Invalid path with `.` prefixed "
+			"segments."
+		);
+		return AVM1Value::nullVal;
+	}
+
+	auto sys = act.getSys();
+
+	auto sharedObj = sys->getAVM1SharedObject(fullPath);
+	// If there's an existing `SharedObject`, return it.
+	if (!sharedObj.isNull())
+		return sharedObj;
+
+	// NOTE: `data` should only exist for `SharedObject`s created by
+	// `get{Local,Remote}()`.
+	auto ctor = act.getPrototypes()->sharedObject->ctor;
+	sharedObj = ctor->construct
+	(
+		act,
+		{}
+	).toObject(act)->as<AVM1SharedObject>();
+
+	sharedObj->name = fullPath;
+
+	// Try to read the `SharedObject`'s data from disk.
+	sharedObj->defineValue("data", *sys->tryReadSharedObject
+	(
+		fullPath
+	).andThen([&](const auto& reader)
+	{
+		// Data exists: Deserialize the data into an `Object`.
+		return makeOptional(deserializeLSO(act, reader));
+	}).orElse([&]
+	{
+		// No data: Create a new `Object`.
+		return NEW_GC_PTR(act.getGcCtx(), AVM1Object
+		(
+			act.getGcCtx(),
+			act.getPrototypes()->object->proto
+		));
+	}), AVM1PropFlags::DontDelete);
+
+	sys->addAVM1SharedObject(fullPath, sharedObj);
+	return sharedObj;
 }
 
 AVM1_FUNCTION_BODY(AVM1SharedObject, getRemote)

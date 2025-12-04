ef8a2772356d9a7f7f0a87b7dcb406364d2bfbba
mrb0nk500 <b0nk@b0nk.xyz>
2025-12-04 18:00:20 -0400

    [AVM1] Implement the new version of `AVM1LoadVars`

---
+++ src/scripting/avm1/toplevel/LoadVars.h
@@ -0,0 +1,74 @@
+/**************************************************************************
+    Lightspark, a free flash player implementation
+
+    Copyright (C) 2025  mr b0nk 500 (b0nk@b0nk.xyz)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#ifndef SCRIPTING_AVM1_TOPLEVEL_LOADVARS_H
+#define SCRIPTING_AVM1_TOPLEVEL_LOADVARS_H 1
+
+#include <utility>
+
+#include "gc/ptr.h"
+#include "scripting/avm1/function.h"
+
+// Based on Ruffle's `avm1::globals::load_vars` crate.
+
+namespace lightspark
+{
+
+class AVM1Activation;
+class AVM1DeclContext;
+class AVM1Object;
+template<typename T>
+class Optional;
+enum RequestMethod;
+
+class AVM1LoadVars
+{
+	using LoadImplPair = Optional<std::pair
+	<
+		GcPtr<AVM1Object>,
+		RequestMethod
+	>>;
+public:
+	static GcPtr<AVM1SystemClass> createClass
+	(
+		AVM1DeclContext& ctx,
+		const GcPtr<AVM1Object>& superProto
+	);
+
+	AVM1_FUNCTION_DECL(load);
+	AVM1_FUNCTION_DECL(send);
+	AVM1_FUNCTION_DECL(sendAndLoad);
+	AVM1_FUNCTION_DECL(decode);
+	AVM1_FUNCTION_DECL(getBytesLoaded);
+	AVM1_FUNCTION_DECL(toString);
+	AVM1_FUNCTION_DECL(onLoad);
+	AVM1_FUNCTION_DECL(onData);
+	AVM1_FUNCTION_DECL(addRequestHeader);
+
+	void loadImpl
+	(
+		AVM1Activation& act,
+		const GcPtr<AVM1Object>& loaderObj,
+		const tiny_string& url,
+		const LoadImplPair& sendObj = {}
+	);
+};
+
+}
+#endif /* SCRIPTING_AVM1_TOPLEVEL_LOADVARS_H */

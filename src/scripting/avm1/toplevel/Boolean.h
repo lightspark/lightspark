67c523a322221c2db460274ea3bb903b6ff8d6b5
mrb0nk500 <b0nk@b0nk.xyz>
2026-05-07 15:45:44 -0300

    [AVM1] Implement `Boolean`

---
+++ src/scripting/avm1/toplevel/Boolean.h
@@ -0,0 +1,65 @@
+/**************************************************************************
+    Lightspark, a free flash player implementation
+
+    Copyright (C) 2026  mr b0nk 500 (b0nk@b0nk.xyz)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#ifndef SCRIPTING_AVM1_TOPLEVEL_BOOLEAN_H
+#define SCRIPTING_AVM1_TOPLEVEL_BOOLEAN_H 1
+
+#include "gc/ptr.h"
+#include "scripting/avm1/function.h"
+#include "scripting/avm1/object/object.h"
+
+// Based on Ruffle's `avm1::globals::boolean` crate.
+
+namespace lightspark
+{
+
+class AVM1Activation;
+class AVM1DeclContext;
+class AVM1SystemClass;
+
+class AVM1Boolean : public AVM1Object
+{
+private:
+	bool value;
+public:
+	AVM1Boolean
+	(
+		const GcContext& ctx,
+		GcPtr<AVM1Object> proto,
+		bool _value = false
+	);
+
+	static GcPtr<AVM1SystemClass> createClass
+	(
+		AVM1DeclContext& ctx,
+		const GcPtr<AVM1Object>& superProto
+	);
+
+	// Implements the `Boolean` constructor.
+	AVM1_FUNCTION_DECL(ctor);
+
+	// Implements `Boolean()`.
+	AVM1_FUNCTION_DECL(boolean);
+
+	AVM1_FUNCTION_TYPE_DECL(AVM1Boolean, toString);
+	AVM1_FUNCTION_TYPE_DECL(AVM1Boolean, valueOf);
+};
+
+}
+#endif /* SCRIPTING_AVM1_TOPLEVEL_BOOLEAN_H */
